c38721bdb0ba25d6e62bf5f357b7fd2353375aa0
AndreaMassimilianoOrfeo <andreamassimilanoorfeo@gmail.com>
2020-05-06 16:05:07 +0200

    lab 1 fine

--- lab1_orfeo/lab1/src/main/java/it/osc/spring/cloud/lab1/controller/TeamController.java
+++ lab1_orfeo/lab1/src/main/java/it/osc/spring/cloud/lab1/controller/TeamController.java
@@ -5,12 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import it.osc.spring.cloud.lab1.model.Team;
 import it.osc.spring.cloud.lab1.repository.TeamRepository;
 
-@RestController
 public class TeamController {
 	
 	@Autowired
--- lab1_orfeo/lab1/src/main/java/it/osc/spring/cloud/lab1/repository/PlayerRepository.java
+++ lab1_orfeo/lab1/src/main/java/it/osc/spring/cloud/lab1/repository/PlayerRepository.java
@@ -3,9 +3,9 @@ package it.osc.spring.cloud.lab1.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import it.osc.spring.cloud.lab1.model.Team;
+import it.osc.spring.cloud.lab1.model.Player;
 
-@RestResource(path="teams", rel="team")
-public interface TeamRepository extends CrudRepository<Team, Long> {
+@RestResource(path="players", rel="player")
+public interface PlayerRepository extends CrudRepository<Player, Long> {
 
 }
